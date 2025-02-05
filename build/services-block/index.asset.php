5f06b499f7a4645c3e5e12675a0e8fe6b01a751a
Jasper Frumau <jasper@imagewize.com>
2025-02-05 12:10:22 +0700

    Service Block Inclusion

--- build/services-block/index.asset.php
+++ build/services-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8b67b3c32f3ddb689c54');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cf899f6b93a2f917d178');
