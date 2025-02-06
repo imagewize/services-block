c1820bf6c1e2c376d8b181845068371c0fc9b7fc
Jasper Frumau <jasper@imagewize.com>
2025-02-06 09:53:20 +0700

    latest build with svg import

--- build/services-block/index.asset.php
+++ build/services-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils'), 'version' => 'd42c23103e5b1eaac0f2');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils'), 'version' => '2f5acc78900111dc77cb');
