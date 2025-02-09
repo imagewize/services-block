01c8d030753659fa7da6a65433175b69b0a06cd6
Jasper Frumau <jasper@imagewize.com>
2025-02-09 09:45:06 +0700

    useBlockProps.save with merged class so WP adds “wp-block-imagewize-services-block"

--- build/services-block/index.asset.php
+++ build/services-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '22bad03175aa9cf22128');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '486ecca4cbd480882274');
