58dc7624542308300ce7be93af4f6068779935e0
Jasper Frumau <jasper@imagewize.com>
2026-04-03 16:26:06 +0700

    Rebuild blocks after @wordpress/scripts upgrade

--- build/services-block/index.asset.php
+++ build/services-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ec48b6e42b6fb23f0ff3');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '042210e888a911d1f89f');
